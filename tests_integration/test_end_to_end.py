dc414589fff0dc1a952890dfd4dc1c4cc3b80f0f
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2019-03-26 15:28:31 -0500

    added integration end to end test for miniseq

--- tests_integration/test_end_to_end.py
+++ tests_integration/test_end_to_end.py
@@ -19,6 +19,7 @@ if len(path_to_module) == 0:
 
 CLEANUP_DIRECTORY_LIST = [
     path.join(path_to_module, "fake_dir_data"),
+    path.join(path_to_module, "fake_miniseq_data"),
     path.join(path_to_module, "fake_ngs_data"),
     path.join(path_to_module, "fake_ngs_data_force"),
     path.join(path_to_module, "fake_ngs_data_nonexistent_project"),
@@ -238,6 +239,100 @@ class TestEndToEnd(unittest.TestCase):
                     self.assertEqual(len(sequence_files), 2)
                     self.assertEqual(sequence_files[0]['fileName'], 'germ_f.fastq.gz')
                     self.assertEqual(sequence_files[1]['fileName'], 'germ_r.fastq.gz')
+
+        self.assertEqual(sample_1_found, True)
+        self.assertEqual(sample_2_found, True)
+        self.assertEqual(sample_3_found, True)
+
+    def test_valid_miniseq_upload(self):
+        """
+        Test a valid directory for upload end to end
+        :return:
+        """
+        # Set our sample config file to use miseq parser and the correct irida credentials
+        self.write_to_config_file(
+            client_id=tests_integration.client_id,
+            client_secret=tests_integration.client_secret,
+            username=tests_integration.username,
+            password=tests_integration.password,
+            base_url=tests_integration.base_url,
+            parser="miniseq"
+        )
+
+        # instance an api
+        test_api = api.ApiCalls(
+            client_id=tests_integration.client_id,
+            client_secret=tests_integration.client_secret,
+            base_url=tests_integration.base_url,
+            username=tests_integration.username,
+            password=tests_integration.password
+        )
+
+        # Create a test project, the uploader does not make new projects on its own
+        # so one must exist to upload samples into
+        # This may not be the project that the files get uploaded to,
+        # but one will be made in the case this is the only test being run
+        project_name = "test_project_4"
+        project_description = "test_project_description_4"
+        project = model.Project(name=project_name, description=project_description)
+        test_api.send_project(project)
+        # We always upload to project "1" so that tests will be consistent no matter how many / which tests are run
+        project_id = "1"
+
+        # Do the upload
+        upload_result = validate_and_upload_single_entry(path.join(path_to_module, "fake_miniseq_data"))
+
+        # Make sure the upload was a success
+        self.assertEqual(upload_result, 0)
+
+        # Verify the files were uploaded
+        sample_list = test_api.get_samples(project_id)
+
+        sample_1_found = False
+        sample_2_found = False
+        sample_3_found = False
+
+        for sample in sample_list:
+            if sample.sample_name in ["01-1111m", "02-2222m", "03-3333m"]:
+                if sample.sample_name == "01-1111m":
+                    sample_1_found = True
+                    sequence_files = test_api.get_sequence_files(project_id, sample.sample_name)
+                    self.assertEqual(len(sequence_files), 2)
+                    res_sequence_file_names = [
+                        sequence_files[0]['fileName'],
+                        sequence_files[1]['fileName']
+                    ]
+                    expected_sequence_file_names = [
+                        '01-1111m_S1_L001_R1_001.fastq.gz',
+                        '01-1111m_S1_L001_R2_001.fastq.gz'
+                    ]
+                    self.assertEqual(res_sequence_file_names.sort(), expected_sequence_file_names.sort())
+                elif sample.sample_name == "02-2222m":
+                    sample_2_found = True
+                    sequence_files = test_api.get_sequence_files(project_id, sample.sample_name)
+                    self.assertEqual(len(sequence_files), 2)
+                    res_sequence_file_names = [
+                        sequence_files[0]['fileName'],
+                        sequence_files[1]['fileName']
+                    ]
+                    expected_sequence_file_names = [
+                        '02-2222m_S1_L001_R1_001.fastq.gz',
+                        '02-2222m_S1_L001_R2_001.fastq.gz'
+                    ]
+                    self.assertEqual(res_sequence_file_names.sort(), expected_sequence_file_names.sort())
+                elif sample.sample_name == "03-3333m":
+                    sample_3_found = True
+                    sequence_files = test_api.get_sequence_files(project_id, sample.sample_name)
+                    self.assertEqual(len(sequence_files), 2)
+                    res_sequence_file_names = [
+                        sequence_files[0]['fileName'],
+                        sequence_files[1]['fileName']
+                    ]
+                    expected_sequence_file_names = [
+                        '03-3333m_S1_L001_R1_001.fastq.gz',
+                        '03-3333m_S1_L001_R2_001.fastq.gz'
+                    ]
+                    self.assertEqual(res_sequence_file_names.sort(), expected_sequence_file_names.sort())
 
         self.assertEqual(sample_1_found, True)
         self.assertEqual(sample_2_found, True)
