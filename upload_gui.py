05eefdf15e0640a70213179e94c91d5d614afbe8
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2019-10-23 15:17:39 -0500

    log crash stack trace to logging directory on crash

--- upload_gui.py
+++ upload_gui.py
@@ -4,16 +4,40 @@ import PyQt5.QtWidgets as QtWidgets
 
 import gui
 
+from appdirs import user_log_dir
+import os
+import traceback
+
+
+def get_crash_log_file_path():
+    # Normal base logging directory name
+    log_directory_name = "irida_uploader"
+    # When running tests, the Makefile creates an environment variable IRIDA_UPLOADER_TEST to 'True'
+    # If it exists then we are running a test and should be logging to the test logs directory
+    if os.environ.get('IRIDA_UPLOADER_TEST'):
+        log_directory_name = "irida_uploader_test"
+    # Use systems default logging path, and append our named directory
+    log_file_path = os.path.join(user_log_dir(log_directory_name), 'crash.log')
+
+    if not os.path.exists(user_log_dir(log_directory_name)):
+        os.makedirs(user_log_dir(log_directory_name))
+
+    return log_file_path
+
 
 def main():
     """
     Entry point for GUI
     :return:
     """
-    app = QtWidgets.QApplication(["IRIDA Uploader"])
-    dlg = gui.MainDialog()
-    dlg.show()
-    sys.exit(app.exec_())
+    try:
+        app = QtWidgets.QApplication(["IRIDA Uploader"])
+        dlg = gui.MainDialog()
+        dlg.show()
+        sys.exit(app.exec_())
+    except Exception:
+        # log full stack trace here
+        traceback.print_exc(file=open(get_crash_log_file_path(), "a"))
 
 
 if __name__ == '__main__':
