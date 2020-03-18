34df05c81e7127e0f1860bb39dc4c12958c6d30c
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2020-03-18 14:37:13 -0500

    added comments to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import setuptools
 
+# Use the readme file as the long description on PyPi
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
@@ -12,6 +13,7 @@ setuptools.setup(
     author_email='jeffrey.thiessen@canada.ca',
     long_description=long_description,
     long_description_content_type="text/markdown",
+    # license specified on github
     license='Apache-2.0',
     keywords="IRIDA NGS uploader",
     packages=setuptools.find_packages(include=['iridauploader',
@@ -23,12 +25,14 @@ setuptools.setup(
                       'cerberus',
                       'argparse',
                       ],
+    # https://pypi.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     include_package_data=True,
+    # Test cases makes make it incompatible with pre 3.5
     python_requires='>=3.5',
 
 )
