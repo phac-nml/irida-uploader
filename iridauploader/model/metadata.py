1595dbdab007993a0500daf0317a0e5147c70955
Darian Hole <darian.hole@canada.ca>
2020-07-10 09:15:11 -0500

    add metadata documentation, fix whitespace

--- iridauploader/model/metadata.py
+++ iridauploader/model/metadata.py
@@ -1,4 +1,4 @@
-class MetadataProfile:
+class OtherMetadata:
 
     def __init__(self, metadata=None):
         if metadata is None:
@@ -8,14 +8,14 @@ class MetadataProfile:
     @property
     def metadata(self):
         return self._metadata
-    
+
     @metadata.setter
     def metadata(self, metadata):
         self._metadata = metadata
-    
+
     def add_metadata_entry(self, key, value):
         self._metadata[key] = value
-    
+
     def get_uploadable_dict(self):
         uploadable_dict = {}
         for key, value in self._metadata.items():
