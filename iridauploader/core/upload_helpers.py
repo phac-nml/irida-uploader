94944a30f86533b29c192aa9be22dd2230057385
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2022-07-12 14:54:19 -0500

    fix pep8

--- iridauploader/core/upload_helpers.py
+++ iridauploader/core/upload_helpers.py
@@ -225,7 +225,7 @@ def upload_sequencing_run(sequencing_run, directory_status, upload_mode, upload_
         logging.error("Lost connection to Irida")
         logging.error("Errors: " + pformat(e.args))
         full_error = "Lost connection to Irida. Errors: " + pformat(e.args)
-        if directory_status.run_id is not None: # A run ID was generated, so the run is partial and can be continued
+        if directory_status.run_id is not None:  # A run ID was generated, so the run is partial and can be continued
             _set_and_write_directory_status(directory_status, DirectoryStatus.PARTIAL, full_error)
         else:
             _set_and_write_directory_status(directory_status, DirectoryStatus.ERROR, full_error)
