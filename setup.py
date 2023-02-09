e0844b10f10afba5e2092b8c03f737760e5a9bde
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2023-02-09 00:13:04 -0600

    version 0.9.0 bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='iridauploader',
-    version='0.8.4',
+    version='0.9.0',
     description='IRIDA uploader: upload NGS data to IRIDA system',
     url='https://github.com/phac-nml/irida-uploader',
     author='Jeffrey Thiessen',
